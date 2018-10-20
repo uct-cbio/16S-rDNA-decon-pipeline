77d3a305f50fce42bd400c80551828d790b35cdf
Kilaza Samson Mwaikono <kilaza@users.noreply.github.com>
2018-10-20 11:28:01 +0200

    Update decont.sh

--- decont.sh
+++ decont.sh
@@ -13,8 +13,7 @@ done >> trans-otu-table.txt
 
 awk 'NR==1; BEGIN{FS=OFS="\t"}{;$6=$6-4; $23=$23-9; $87=$87-1}1' trans-otus-table.txt | sed '2d'|awk '$6<0 {$6=0}; $23<0 {$23=0}; $87<0 {$87=0}1' OFS='\t' > decont-table.txt
 
-## NOTE: $6 and $23 and $87 are the columns of the OTUS in the target biological sample otu-table which mapped to the contaminant OTUs, while 4, 9 and 1 are their respective average reads in the background of the spiked controls 
-
+## NOTE: $6 and $23 and $87 are the columns of the OTUs in the target biological sample otu-table which mapped to the contaminant OTUs, while 4, 9 and 1 are their respective average reads in the background of the spiked controls 
 
 ## Transposing otu-table to qiime compatible format
 
@@ -24,3 +23,4 @@ for (( i=1; i<=rowcount; i++ )); do
     awk -v i="$i" -F " " '{printf("%s\t ", $i)}' decont-table.txt
     echo
 done >> final-otu-table.txt ## This table can now be used in the downstream processing such as the BIOM file generation
+
